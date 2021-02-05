5605c09544497d9b1ca03007e9f91b3ba2d492e8
yokonao <yokotukanao@me.com>
2021-02-05 23:23:02 +0900

    分割コンパイル

---
+++ hooligan.h
@@ -0,0 +1,61 @@
+#ifndef HOOLIGAN_H
+#define HOOLIGAN_H
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Tokenizer
+typedef enum
+{
+    TK_OPERATOR,
+    TK_NUMBER,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+Token *token;
+
+struct Token
+{
+    TokenKind kind;
+    Token *next;
+    int value;
+    char *string;
+};
+
+void error(char *fmt, ...);
+bool consume(char op);
+void expect(char op);
+int expect_number();
+
+Token *tokenize();
+
+// Parser
+typedef enum
+{
+    ND_NUM,
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *expr();
+void gen(Node *node);
+
+#endif
---
+++ parser.c
@@ -0,0 +1,134 @@
+#include "hooligan.h"
+
+
+Node *expr();
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *num()
+{
+    return new_node_num(expect_number());
+}
+
+Node *primary()
+{
+    if (consume('('))
+    {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    else
+    {
+        return num();
+    }
+}
+
+Node *unary()
+{
+    if (consume('+'))
+    {
+        return primary();
+    }
+    else if (consume('-'))
+    {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
+}
+
+Node *mul()
+{
+    Node *node = unary();
+    for (;;)
+    {
+        if (consume('*'))
+        {
+            node = new_node(ND_MUL, node, unary());
+        }
+        else if (consume('/'))
+        {
+            node = new_node(ND_DIV, node, unary());
+        }
+        else
+        {
+            return node;
+        }
+    }
+}
+
+Node *expr()
+{
+    Node *node = mul();
+    for (;;)
+    {
+        if (consume('+'))
+        {
+            node = new_node(ND_ADD, node, mul());
+        }
+        else if (consume('-'))
+        {
+            node = new_node(ND_SUB, node, mul());
+        }
+        else
+        {
+            return node;
+        }
+    }
+}
+
+void gen(Node *node)
+{
+    if (node->kind == ND_NUM)
+    {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("  pop rdi\n");
+        printf("  pop rax\n");
+        printf("  add rax, rdi\n");
+        printf("  push rax\n");
+        break;
+    case ND_SUB:
+        printf("  pop rdi\n");
+        printf("  pop rax\n");
+        printf("  sub rax, rdi\n");
+        printf("  push rax\n");
+        break;
+    case ND_MUL:
+        printf("  pop rdi\n");
+        printf("  pop rax\n");
+        printf("  imul rax, rdi\n");
+        printf("  push rax\n");
+        break;
+    case ND_DIV:
+        printf("  pop rdi\n");
+        printf("  pop rax\n");
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        printf("  push rax\n");
+        break;
+    }
+}
