58fde2e64daa5bd1cf79e8630d333befc5e7ae9e
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-02-10 16:25:24 +0900

    add : 不要なアセンブリを削除

--- main.c
+++ main.c
@@ -17,11 +17,9 @@ int main(int argc, char **argv)
     printf("main:\n");
     printf("  push rbp\n");
     printf("  mov rbp, rsp\n");
-    printf("  sub rsp, 208\n");
     Node *node = stmt();
     gen(node);
     printf("  pop rax\n");
-    printf("  mov rsp, rbp\n");
     printf("  pop rbp\n");
     printf("  ret\n");
     return 0;
