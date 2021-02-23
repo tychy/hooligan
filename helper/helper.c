5b5dee3ac20bc36e7a4c1cb50110bdda05484fdb
yokonao <yokotukanao@me.com>
2021-02-23 23:54:42 +0900

    テストを修正

--- helper/helper.c
+++ helper/helper.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
 
-int foo() { return 10; }
-
 int hello_world()
 {
     printf("Hello World!\n");
     return 1;
-}
-
-int two(int x)
-{
-    return 2 * x;
-}
-
-int BIG(int x)
-{
-    return x + 10;
 }
 
 int add(int x, int y)
