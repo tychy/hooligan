b6badb66516b7af748c8358eb5485f17f01ea6f9
yokonao <yokotukanao@me.com>
2021-02-28 03:32:41 +0900

    Cファイルによるテストを追加

---
+++ test/ctrl.c
@@ -0,0 +1,27 @@
+int main()
+{
+    int a;
+
+    a = 0;
+    int counter;
+    for (counter = 0; counter < 100; counter = counter + 1)
+        if (counter * counter == 400)
+            a = counter;
+    if (a != 20)
+        return 1;
+
+    a = 0;
+    int i;
+    i = 0;
+    while (i < 10)
+        if (i == 5)
+        {
+            a = i;
+            i = i + 1;
+        }
+        else
+            i = i + 1;
+    if (a != 5)
+        return 1;
+    return 0;
+}
