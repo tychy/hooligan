63d6e461550625e31263411d5526d0ee5437c12a
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-03-10 18:48:02 +0900

    add : arrayをメンバにもつテスト

--- test/struct_from_func.c
+++ test/struct_from_func.c
@@ -7,6 +7,7 @@ int teststruct()
         int offset;
     } Hello;
     Hello.val = 10;
+    Hello.offset = 32;
     if (Hello.val != 10)
     {
         return 1;
@@ -15,7 +16,37 @@ int teststruct()
     return 10 + Hello.val;
 }
 
+int teststructarray()
+{
+    struct
+    {
+        int val;
+        int offset;
+        int a[10];
+    } b;
+    b.val = 12;
+    b.offset = 2;
+    for (int i = 0; i < 10; i = i + 1)
+    {
+        b.a[i] = i + 1;
+    }
+
+    int res = 0;
+    for (int i = 0; i < 10; i = i + 1)
+    {
+        res = res + b.a[i];
+    }
+    return res;
+}
 int main()
 {
-    return teststruct();
+    if (20 != teststruct())
+    {
+        return 1;
+    }
+    if (55 != teststructarray())
+    {
+        return 1;
+    }
+    return 0;
 }
