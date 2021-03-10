d2124bfe9940bd53895d73581096e3090bacecd5
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-03-11 01:54:48 +0900

    add : read ->

--- tokenizer.c
+++ tokenizer.c
@@ -2,7 +2,7 @@
 
 // note: 文字数の多いものを先に登録する
 // note: 要素数を更新する
-static char *operator_list[21] = {
+static char *operator_list[22] = {
     "==",
     "!=",
     ">=",
@@ -24,6 +24,7 @@ static char *operator_list[21] = {
     "[",
     "]",
     ".",
+    "->",
 };
 
 static int operator_list_count = sizeof(operator_list) / sizeof(operator_list[0]);
