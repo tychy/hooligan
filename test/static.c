26557b8eec24e660ed66b443dace1d5cff08d5a2
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-03-12 03:45:29 +0900

    add: charとptr型へのstaticのテスト

--- test/static.c
+++ test/static.c
@@ -20,7 +20,45 @@ int testStaticImplicit()
     return 0;
 }
 
-/*
+int impl_char_add()
+{
+    static char x;
+    x += 5;
+    return x;
+}
+
+int testStaticImplicitChar()
+{
+    char res = impl_char_add();
+    if (res != 5)
+    {
+        return 1;
+    }
+    res = impl_char_add();
+    if (res != 10)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int a;
+int impl_ptr_add()
+{
+    static int *x;
+    x = &a;
+    a = 3;
+    return *x;
+}
+int testStaticImplicitPtr()
+{
+    int res = impl_ptr_add();
+    if (res != 3)
+    {
+        return 1;
+    }
+    return 0;
+}
 int expl_add()
 {
     static int x = 5;
@@ -43,7 +81,6 @@ int testStaticExplicit()
     return 0;
 }
 
-*/
 int impl_if()
 {
     int hoge = 3;
@@ -84,11 +121,20 @@ int main()
         return 1;
     }
 
-    /*
     if (testStaticExplicit() != 0)
     {
         return 1;
     }
-    */
+
+    if (testStaticImplicitPtr() != 0)
+    {
+        return 1;
+    }
+
+    if (testStaticImplicitChar() != 0)
+    {
+        return 1;
+    }
+
     return 0;
 }
