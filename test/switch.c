47f87c910f41c441b8c8c78f5cdbdd572ea5544e
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-03-16 17:54:48 +0900

    add : 複雑なテスト

--- test/switch.c
+++ test/switch.c
@@ -96,35 +96,94 @@ int testSwitchCaseEasyE()
 int testSwitchCaseMidA()
 {
     int i;
+    int sums = 0;
     for (i = 0; i < 10; i++)
     {
         switch (i)
         {
         case 0:
         case 5:
-            continue;
         case 9:
+            sums += 1;
             break;
+        case 10:
+            sums = 0;
         }
-        printf("%d\n", i);
     }
-    if (i != 10)
+    if (sums != 3)
     {
         return 1;
     }
     return 0;
 }
+int testSwitchCaseMidB()
+{
+    int i;
+    int sums = 0;
+    for (i = 0; i < 11; i++)
+    {
+        switch (i)
+        {
+        case 0:
+        case 5:
+        case 9:
+            sums += 1;
+            break;
+        case 10:
+            sums = 100;
+        }
+    }
+    if (sums != 100)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int testSwitchCaseMidC()
+{
+    int i = 7;
+    int x = 11;
+    int res = 0;
+    switch (i)
+    {
+    case 0:
+        res = 0;
+        break;
+    case 5:
+        res = 10;
+        res = 5;
+        break;
+    case 7:
+        res = 10;
+        res = 7;
+        break;
+    case 9:
+        res = 9;
+        break;
+    case 10:
+        res = 10;
+        break;
+    }
+    if (res != 7)
+    {
+        return 1;
+    }
+    return 0;
+}
+
 int main()
 {
-    /*
     if (testSwitchCaseEasyA() != 0)
     {
         return 1;
     }
+
     if (testSwitchCaseEasyB() != 0)
     {
         return 1;
     }
+
     if (testSwitchCaseEasyC() != 0)
     {
         return 1;
@@ -134,14 +193,26 @@ int main()
     {
         return 1;
     }
+
     if (testSwitchCaseEasyE() != 0)
     {
         return 1;
     }
-*/
+
     if (testSwitchCaseMidA() != 0)
     {
         return 1;
     }
+
+    if (testSwitchCaseMidB() != 0)
+    {
+        return 1;
+    }
+
+    if (testSwitchCaseMidC() != 0)
+    {
+        return 1;
+    }
+
     return 0;
 }
