c5c657ae16f22e5303e1d6b62c06e7a329ddce45
yokonao <yokotukanao@me.com>
2021-03-16 14:41:07 +0000

    新テストを追加

---
+++ tests/basic/main.c
@@ -0,0 +1,123 @@
+int fibo(int index)
+{
+    if (index == 0)
+        return 0;
+    if (index == 1)
+        return 1;
+    return fibo(index - 1) + fibo(index - 2);
+}
+int testFibo()
+{
+    if (fibo(0) != 0)
+        return 1;
+    if (fibo(1) != 1)
+        return 1;
+    if (fibo(2) != 1)
+        return 1;
+    if (fibo(3) != 2)
+        return 1;
+    if (fibo(4) != 3)
+        return 1;
+    if (fibo(5) != 5)
+        return 1;
+    if (fibo(6) != 8)
+        return 1;
+    if (fibo(7) != 13)
+        return 1;
+    if (fibo(8) != 21)
+        return 1;
+    if (fibo(9) != 34)
+        return 1;
+    if (fibo(10) != 55)
+        return 1;
+    if (fibo(11) != 89)
+        return 1;
+    return 0;
+}
+
+int testMinus()
+{
+    int x = -5;
+    if (x != -5)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int testCalc()
+{
+    int a;
+    a = 1 + 2 + 3;
+    if (a != 6)
+        return 1;
+    a = (4 * 9) - (6 * 5) / 7;
+    if (a == 32)
+        a = 0;
+    else
+        return 1;
+    a = 4 * 5 * 2 + 6 * 7 - 5 * 9;
+    if (a != 37)
+        return 1;
+
+    a = (4 > 1) > (1 < 0);
+    if (a != 1)
+        return 1;
+    a -= 41;
+    if (a != -40)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int testPtr()
+{
+    int a = 10001;
+    int b = 10002;
+    int c = 20001;
+    int d = 20002;
+    int e = 40000;
+
+    int *p;
+    int *q;
+    p = &a;
+    if (*p != 10001)
+        return 1;
+    q = p - 2;
+    if (*q != 20001)
+        return 1;
+    q = p - 3;
+    if (*q != 20002)
+        return 1;
+    if (*(q - 1) != 40000)
+        return 1;
+    if (*(q + 2) != 10002)
+        return 1;
+    if (*(p - 1) + *(p - 2) + *(p - 3) != 50005)
+        return 1;
+    return 0;
+}
+
+int main()
+{
+
+    if (testFibo() != 0)
+    {
+        return 1;
+    }
+    if (testMinus() != 0)
+    {
+        return 1;
+    }
+    if (testCalc() != 0)
+    {
+        return 1;
+    }
+    if (testPtr() != 0)
+    {
+        return 1;
+    }
+
+    return 0;
+}
