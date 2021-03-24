c3a5526d52eb35e8cd46b932cd4a504afdc3ca55
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-03-24 20:19:08 +0900

    add : globalな構造体のテスト

--- tests/struct/main.c
+++ tests/struct/main.c
@@ -13,7 +13,7 @@ int testStruct()
     return 0;
 }
 
-int testStructTag()
+int testStructTagLocal()
 {
     struct hoge
     {
@@ -28,13 +28,37 @@ int testStructTag()
     }
     return 0;
 }
+
+struct foo
+{
+    int s;
+    int t;
+};
+
+int testStructTagGlobal()
+{
+    struct foo x;
+    x.s = 10;
+    if (x.s != 10)
+    {
+        return 1;
+    }
+    return 0;
+}
+
 int main()
 {
     if (testStruct() != 0)
     {
         return 1;
     }
-    if (testStructTag() != 0)
+
+    if (testStructTagLocal() != 0)
+    {
+        return 1;
+    }
+
+    if (testStructTagGlobal() != 0)
     {
         return 1;
     }
