e55516a3a05dfc31cfb65a13ebd1b5e95ab18486
yokonao <yokotukanao@me.com>
2021-03-30 09:07:44 +0000

    テストを統合

--- tests/struct/main.c
+++ tests/struct/main.c
@@ -135,6 +135,104 @@ int testTypedefStructPtr()
     return 0;
 }
 
+int testStructsCircularRefLocal()
+{
+    typedef struct Left Left;
+    typedef struct Right Right;
+    struct Left
+    {
+        Right *rev;
+        int val;
+    };
+    struct Right
+    {
+        Left *rev;
+        int val;
+    };
+    Left l;
+    Right r;
+    l.val = 10;
+    r.val = 20;
+    l.rev = &r;
+    r.rev = &l;
+    if (l.val != 10)
+    {
+        return 1;
+    }
+    if (r.val != 20)
+    {
+        return 1;
+    }
+    if (l.rev->val != 20)
+    {
+        return 1;
+    }
+    if (l.rev->rev->val != 10)
+    {
+        return 1;
+    }
+    if (r.rev->val != 10)
+    {
+        return 1;
+    }
+    if (r.rev->rev->val != 20)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+typedef struct Up Up;
+typedef struct Down Down;
+
+struct Up
+{
+    Down *rev;
+    int val;
+};
+
+struct Down
+{
+    Up *rev;
+    int val;
+};
+
+int testStructsCircularRefGlobal()
+{
+    Up u;
+    Down d;
+    u.val = 10;
+    d.val = 20;
+    u.rev = &d;
+    d.rev = &u;
+    if (u.val != 10)
+    {
+        return 1;
+    }
+    if (d.val != 20)
+    {
+        return 1;
+    }
+    if (u.rev->val != 20)
+    {
+        return 1;
+    }
+    if (u.rev->rev->val != 10)
+    {
+        return 1;
+    }
+    if (d.rev->val != 10)
+    {
+        return 1;
+    }
+    if (d.rev->rev->val != 20)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
 int testTypedefStructSelf()
 {
     typedef struct Graph Graph;
@@ -204,5 +302,16 @@ int main()
     {
         return 1;
     }
+
+    if (testStructsCircularRefLocal() != 0)
+    {
+        return 1;
+    }
+
+    if (testStructsCircularRefGlobal() != 0)
+    {
+        return 1;
+    }
+
     return 0;
 }
