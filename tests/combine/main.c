8c58e6ceb56f1228d1062e34dfcfa5471123a788
yokonao <yokotukanao@me.com>
2021-03-30 09:05:56 +0000

    Globalバージョンもパスする

--- tests/combine/main.c
+++ tests/combine/main.c
@@ -1,4 +1,4 @@
-int main()
+int testStructsCircularRefLocal()
 {
     typedef struct Left Left;
     typedef struct Right Right;
@@ -39,6 +39,69 @@ int main()
         return 1;
     }
     if (r.rev->rev->val != 20)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+typedef struct Up Up;
+typedef struct Down Down;
+
+struct Up
+{
+    Down *rev;
+    int val;
+};
+
+struct Down
+{
+    Up *rev;
+    int val;
+};
+
+int testStructsCircularRefGlobal()
+{
+    Up u;
+    Down d;
+    u.val = 10;
+    d.val = 20;
+    u.rev = &d;
+    d.rev = &u;
+    if (u.val != 10)
+    {
+        return 1;
+    }
+    if (d.val != 20)
+    {
+        return 1;
+    }
+    if (u.rev->val != 20)
+    {
+        return 1;
+    }
+    if (u.rev->rev->val != 10)
+    {
+        return 1;
+    }
+    if (d.rev->val != 10)
+    {
+        return 1;
+    }
+    if (d.rev->rev->val != 20)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+int main()
+{
+    if (testStructsCircularRefLocal() != 0)
+    {
+        return 1;
+    }
+    if (testStructsCircularRefGlobal() != 0)
     {
         return 1;
     }
