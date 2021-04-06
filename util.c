d1ff29a22b14535ca5f7718ccffbab721cd56eb1
yokonao <yokotukanao@me.com>
2021-04-06 09:53:21 +0000

    refactor join_str

--- util.c
+++ util.c
@@ -26,21 +26,17 @@ void error2(char *fmt, char *v1, char *v2)
     exit(1);
 }
 
-char *join_str(char *pre, char *post)
-{
-    size_t pre_size = strlen(pre);
-    size_t post_size = strlen(post);
-    char *res = calloc(1, pre_size + post_size + 2);
-    memcpy(res, pre, pre_size);
-    memcpy(res + pre_size, post, post_size);
-    return res;
-}
-
 char *insert_str(char *src, int pos, char *target)
 {
     char *res = calloc(1, strlen(src) + strlen(target) + 1);
     memcpy(res, src, pos);
     memcpy(res + pos, target, strlen(target));
     memcpy(res + pos + strlen(target), src + pos, strlen(src) - pos);
+    return res;
+}
+
+char *join_str(char *pre, char *post)
+{
+    char *res = insert_str(pre, strlen(pre), post);
     return res;
 }
