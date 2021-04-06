cd00bbd59ef18ed9f3f05ab4a4f73072eb87df5c
yokonao <yokotukanao@me.com>
2021-04-06 08:57:41 +0000

    add join_str

--- unittest/utiltest.c
+++ unittest/utiltest.c
@@ -4,5 +4,9 @@ int main()
 {
     if (not(true))
         return 1;
+    char *s1 = "hogehoge";
+    char *s2 = "fugafuga";
+    char *s3 = join_str(s1, s2);
+    printf("%s\n", s3);
     return 0;
 }
--- util.c
+++ util.c
@@ -25,3 +25,13 @@ void error2(char *fmt, char *v1, char *v2)
     fprintf(stderr, "\n");
     exit(1);
 }
+
+char *join_str(char *pre, char *post)
+{
+    size_t pre_size = strlen(pre);
+    size_t post_size = strlen(post);
+    char *res = calloc(1, pre_size + post_size + 2);
+    memcpy(res, pre, pre_size);
+    memcpy(res + pre_size, post, post_size);
+    return res;
+}
