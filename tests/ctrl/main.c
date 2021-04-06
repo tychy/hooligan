f87828069c2b4489ff6a7e41e77a847f72ad9679
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-04-07 00:11:35 +0900

    add : デクリメントのテスト

--- tests/ctrl/main.c
+++ tests/ctrl/main.c
@@ -197,6 +197,120 @@ int testPostIncrementStruct()
     return 0;
 }
 
+int testPostDecrement()
+{
+    int sum_one = 0;
+    int sum_two = 0;
+    int cnt = 99;
+    for (int i = 0; i < 100; i++)
+    {
+        // 両者同じ値が足されるはず
+        sum_one += i;
+        sum_two += cnt--;
+    }
+    if (cnt != -1)
+    {
+        return 1;
+    }
+    if (sum_one != 4950)
+    {
+        return 1;
+    }
+    if (sum_two != 4950)
+    {
+        return 1;
+    }
+    return 0;
+}
+int testPostDecrementIntPtr()
+{
+
+    int x[4] = {1, 2, 3, 4};
+    int *y = x + 3;
+    if (*y != 4)
+    {
+        return 1;
+    }
+    if (*y-- != 4)
+    {
+        return 1;
+    }
+    if (*y != 3)
+    {
+        return 1;
+    }
+    y--;
+    if (*y != 2)
+    {
+        return 1;
+    }
+    return 0;
+}
+int testPostDecrementCharPtr()
+{
+    char *y = "abcde";
+    char *x = y + 4;
+    if (*x != 'e')
+    {
+        return 1;
+    }
+    x--;
+    if (*x != 'd')
+    {
+        return 1;
+    }
+    if (*x-- != 'd')
+    {
+        return 1;
+    }
+    if (*x != 'c')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int testPostDecrementStruct()
+{
+    typedef struct Hoge Hoge;
+    struct Hoge
+    {
+        int val;
+    };
+    Hoge x;
+    Hoge *y;
+    y = calloc(1, sizeof(Hoge));
+    y->val = 100;
+    x.val = 10;
+    if (x.val != 10)
+    {
+        return 1;
+    }
+
+    if (x.val-- != 10)
+    {
+        return 1;
+    }
+    if (x.val != 9)
+    {
+        return 1;
+    }
+
+    if (y->val != 100)
+    {
+        return 1;
+    }
+
+    if (y->val-- != 100)
+    {
+        return 1;
+    }
+    if (y->val != 99)
+    {
+        return 1;
+    }
+    return 0;
+}
 int main()
 {
     if (testCtrl() != 0)
@@ -227,6 +341,19 @@ int main()
     {
         return 1;
     }
+    if (testPostDecrement() != 0)
+    {
+        return 1;
+    }
 
+    if (testPostDecrementIntPtr() != 0)
+    {
+        return 1;
+    }
+
+    if (testPostDecrementCharPtr() != 0)
+    {
+        return 1;
+    }
     return 0;
 }
