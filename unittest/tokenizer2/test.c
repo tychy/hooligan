091d01c424e3cc621c99fc297516b5f2adec64ca
yokonao <yokotukanao@me.com>
2021-05-25 10:39:26 +0000

    プリプロセッシングトークナイズのテストを1つ追加 - charcter-constに関係する

--- unittest/tokenizer2/test.c
+++ unittest/tokenizer2/test.c
@@ -5,6 +5,7 @@ void dump_pp_token(PPToken *tok);
 
 int main()
 {
-    dump_pp_token(decompose_to_pp_token(read_file("unittest/tokenizer2/sample.c")));
+    dump_pp_token(decompose_to_pp_token(read_file("unittest/tokenizer2/include/1.c")));
+    dump_pp_token(decompose_to_pp_token(read_file("unittest/tokenizer2/include/2.c")));
     return 0;
 }
