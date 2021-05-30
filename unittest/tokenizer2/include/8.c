98a7305c8b442492175429b95eea1933aba21035
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-05-30 16:13:39 +0900

    マクロの二段階展開のテストを追加

---
+++ unittest/tokenizer2/include/8.c
@@ -0,0 +1,9 @@
+#define bb bbb
+#define b bb
+
+int main()
+{
+    int b = 0;
+    bb = 110;
+    return 0;
+}
--- unittest/tokenizer2/test.c
+++ unittest/tokenizer2/test.c
@@ -13,6 +13,7 @@ void testPPToken()
     dump_pp_token(decompose_to_pp_token(read_file("unittest/tokenizer2/include/5.c")));
     dump_pp_token(decompose_to_pp_token(read_file("unittest/tokenizer2/include/6.c")));
     dump_pp_token(decompose_to_pp_token(read_file("unittest/tokenizer2/include/7.c")));
+    dump_pp_token(decompose_to_pp_token(read_file("unittest/tokenizer2/include/8.c")));
     return;
 }
 
@@ -29,6 +30,10 @@ void testMacro()
     pp_ctx = calloc(1, sizeof(PPContext));
     pp_ctx->macros = NULL;
     dump_pp_token(preprocess_macro(decompose_to_pp_token(read_file("unittest/tokenizer2/include/7.c"))));
+
+    pp_ctx = calloc(1, sizeof(PPContext));
+    pp_ctx->macros = NULL;
+    dump_pp_token(preprocess_macro(decompose_to_pp_token(read_file("unittest/tokenizer2/include/8.c"))));
 
     return;
 }
