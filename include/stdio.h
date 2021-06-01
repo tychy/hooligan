98a54b1f1790d6e682ae8364eabbac1e4f510582
yokonao <yokotukanao@me.com>
2021-06-01 13:14:41 +0000

    独自のヘッダファイルにdefine文を追加

--- include/stdio.h
+++ include/stdio.h
@@ -1,3 +1,9 @@
 extern struct _IO_FILE *stdin;		/* Standard input stream.  */
 extern struct _IO_FILE *stdout;		/* Standard output stream.  */
 extern struct _IO_FILE *stderr;		/* Standard error output stream.  */
+#define SEEK_SET	0	/* Seek from beginning of file.  */
+#define SEEK_CUR	1	/* Seek from current position.  */
+#define SEEK_END	2	/* Seek from end of file.  */
+#define FILE int
+#define size_t int
+#define NULL 0
