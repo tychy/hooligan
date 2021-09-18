c7df4b1f3dce433fbc7eabe23712eab449799516
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-09-18 18:47:12 +0900

    char型のconstのテストを追加

--- tests/const/main.c
+++ tests/const/main.c
@@ -8,9 +8,24 @@ int testConstInt()
     return 0;
 }
 
+int testConstChar()
+{
+    const char x = 10;
+    if (x != 10)
+    {
+        return 1;
+    }
+    return 0;
+}
+
 int main()
 {
     if (testConstInt() != 0)
+    {
+        return 1;
+    }
+
+    if (testConstChar() != 0)
     {
         return 1;
     }
