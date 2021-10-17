9d12bbabb6bc1a8070e1be400d780ff45f2a412a
yokonao <yokotukanao@me.com>
2021-10-17 16:54:55 +0000

    最適化の後にcontinueする

--- src/optimizer.c
+++ src/optimizer.c
@@ -14,10 +14,10 @@ ILSentence *optimize_inter_language(ILSentence *head)
             {
                 ILSentence *pop = next->next;
                 bool is_same_reg = push->first_operand->ty == pop->first_operand->ty && push->first_operand->reg == pop->first_operand->reg;
+
                 if (is_same_reg && pop->next)
                 {
                     cur->next = pop->next;
-                    cur = head;
                     continue;
                 }
             }
